b7749eba631ec40c9a3adf2a9ef1d1916ac79e0f
Pushkar Chaturvedi <131661389+PushkarOM@users.noreply.github.com>
2026-09-22 08:30:52 +0530

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+
+        priority_queue<int> pq(stones.begin(), stones.end());
+
+        int x, y;
+
+        while(pq.size() > 1){
+            y = pq.top();
+            pq.pop();
+            x = pq.top();
+            pq.pop();
+
+            if(x < y){
+                pq.push(y-x);
+            }
+            else if(x == y){
+                pq.push(0);
+            }
+
+        }
+
+        return pq.top();
+    }
+};
