435c317d44681e2d443c8c30258a33fb64d56dd3
Pushkar Chaturvedi <131661389+PushkarOM@users.noreply.github.com>
2026-09-25 12:08:01 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void backtrack(vector<vector<int>>& result, vector<int> curr, vector<int>& used, vector<int>& nums){
+
+        if(curr.size() == nums.size()){
+            result.push_back(curr);
+            return;
+        }
+
+        for(int i = 0; i <  nums.size(); i++){
+            if(used[i] == 1) continue;
+
+            used[i] = 1;
+            curr.push_back(nums[i]);
+
+            backtrack(result, curr, used, nums);
+
+            curr.pop_back();
+            used[i] = 0;
+        }
+
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> used(nums.size(),0);
+        vector<int> curr;
+
+        backtrack(result, curr, used, nums);
+
+        return result;
+    }
+};
