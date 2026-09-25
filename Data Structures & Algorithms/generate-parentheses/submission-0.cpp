9655a29c511efff3c1413b1db7a47e3b98c8b189
Pushkar Chaturvedi <131661389+PushkarOM@users.noreply.github.com>
2026-09-25 12:33:05 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    void backtrack(vector<string>& result, string curr, int n, int open, int close){
+        if(curr.length() == 2 * n){
+            result.push_back(curr);
+            return;
+        }
+
+        if(open < n) {
+            curr.push_back('(');
+            backtrack(result, curr, n, open + 1, close);
+            curr.pop_back();
+        }
+
+        // Add ')' only if there is an unmatched '('
+        if(close < open) {
+            curr.push_back(')');
+            backtrack(result, curr, n, open, close + 1);
+            curr.pop_back();
+        }
+
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string curr;
+        
+        backtrack(result, curr, n,  0, 0);
+
+        return result;
+    }
+};
